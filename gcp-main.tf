b4f74842fcd5f218ace3811fba00d4d4b8c9d2c2
rodlast <rodrigo_fior@yahoo.com.br>
2022-03-24 17:52:24 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -6,7 +6,7 @@ resource "google_app_engine_application" "app" {
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "us-central1"
+  location = "us-west1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
